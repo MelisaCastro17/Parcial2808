31fafe207dbec374b4e211708fd3950b7c5a5962
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-08-28 21:43:36 -0500

    planetas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,51 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Parcial2808';
+  item:string=' ';
+  dist:string=' ';
+  lun:string =' ';
+  mas:string='';
+    planetas:any = [
+      {item: 'Mercurio'},
+      {item: 'Venus'},
+      {item: 'Tierra'},
+      {item: 'Marte'},
+      {item: 'Júpiter'},
+      {item: 'Saturno'},
+      {item: 'Urano'},
+      {item: 'Neptuno'}
+    ];
+    distancia:any = [
+      {dist: '51.91'},
+      {dist: '51.92'},
+      {dist: '51.93'},
+      {dist: '51.94'},
+      {dist: '51.95'},
+      {dist: '51.96'},
+      {dist: '51.97'},
+      {dist: '51.98'}
+    ];
+
+    lunas:any = [
+      {lun: 'luna1'},
+      {lun: 'luna2'},
+      {lun: 'luna3'},
+      {lun: 'luna4'},
+      {lun: 'luna5'},
+      {lun: 'luna6'},
+      {lun: 'luna7'},
+      {lun: 'luna8'}
+    ];
+    masa:any = [
+      {mas: 'masa1'},
+      {mas: 'masa2'},
+      {mas: 'masa3'},
+      {mas: 'masa4'},
+      {mas: 'masa5'},
+      {mas: 'masa6'},
+      {mas: 'masa7'},
+      {mas: 'masa8'}
+    ];
 }
